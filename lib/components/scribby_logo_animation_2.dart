38a5e23fc2608cfccd5ac306b82b5bc28063e4c8
Sam Century <sam100years@hotmail.com>
2024-08-25 18:26:39 -0400

    removed logo animation

--- lib/components/scribby_logo_animation_2.dart
+++ lib/components/scribby_logo_animation_2.dart
@@ -7,66 +7,54 @@ class ScribbyLogoAnimation2 extends StatefulWidget {
   State<ScribbyLogoAnimation2> createState() => _ScribbyLogoAnimation2State();
 }
 
-class _ScribbyLogoAnimation2State extends State<ScribbyLogoAnimation2>  with SingleTickerProviderStateMixin{
+class _ScribbyLogoAnimation2State extends State<ScribbyLogoAnimation2> {
 
-  late final AnimationController _slideController = AnimationController(
-    vsync: this,
-    duration: const Duration(milliseconds: 1000),
-  )..forward();
+  // late final AnimationController _slideController = AnimationController(
+  //   vsync: this,
+  //   duration: const Duration(milliseconds: 1000),
+  // )..forward();
 
-  late final Animation<Offset> _positionAnimation;
-  late final Animation<double> _opacityAnimation;
+  // late final Animation<Offset> _positionAnimation;
+  // late final Animation<double> _opacityAnimation;
 
-  @override
-  void initState() {
+  // @override
+  // void initState() {
 
-    super.initState();
-    _positionAnimation = Tween<Offset>(
-      begin: const Offset(0.0, 1.0),
-      end: Offset.zero,
-    ).animate(CurvedAnimation(
-      parent: _slideController, 
-      curve: Curves.easeInOutQuint
-      )
-    );
+  //   super.initState();
+  //   _positionAnimation = Tween<Offset>(
+  //     begin: const Offset(0.0, 1.0),
+  //     end: Offset.zero,
+  //   ).animate(CurvedAnimation(
+  //     parent: _slideController, 
+  //     curve: Curves.easeInOutQuint
+  //     )
+  //   );
 
 
-    /// OPACITY
-    _opacityAnimation = TweenSequence<double>([
-      TweenSequenceItem(
-        tween: Tween<double>(begin: 0.0, end: 1.0), 
-        weight: 1.0
-      ),              
-    ]).animate(CurvedAnimation(
-      parent: _slideController, 
-      curve: Curves.easeInOutQuint
-      )
-    );
-  }
+  //   /// OPACITY
+  //   _opacityAnimation = TweenSequence<double>([
+  //     TweenSequenceItem(
+  //       tween: Tween<double>(begin: 0.0, end: 1.0), 
+  //       weight: 1.0
+  //     ),              
+  //   ]).animate(CurvedAnimation(
+  //     parent: _slideController, 
+  //     curve: Curves.easeInOutQuint
+  //     )
+  //   );
+  // }
 
-  @override
-  void dispose() {
-    _slideController.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   // _slideController.dispose();
+  //   super.dispose();
+  // }
 
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _slideController,
-      builder: (context,child)  {
-        return AnimatedOpacity(
-          opacity: _opacityAnimation.value,
-          duration: const Duration(milliseconds: 1000),
-          child: SlideTransition(
-            position: _positionAnimation,
-            child: Image(
-              image: AssetImage('assets/images/scribby_label_1.png')
-            )
-          ),
-        );
-      },
+    return Image(
+      image: AssetImage('assets/images/scribby_label_1.png')
     );
   }
 }
